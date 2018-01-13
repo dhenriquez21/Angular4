7f841916c4b68b662a94ccf59d47bd76f0899acf
dhenriquez21 <35388148+dhenriquez21@users.noreply.github.com>
2018-01-13 00:28:01 -0500

    CRUD FIREBASE
    CREATE
    DELETE
    READ
    UPDATE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,36 +8,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  ////VARIABLES///
   title:string = 'Angular4';
   name:string='';
-  grilla:Array<object>=[];
+  //grilla:Array<object>=[];
   showform:boolean=false;
   crearsh:boolean=false;
   editarsh:boolean=false;
-  json:object = {};
+  json:object = {id:null};
+  list:Array<object>=[];
+  grilla: Observable<any[]>;
+
+
+  ////FUNCIONES///
+  constructor(public afDB: AngularFireDatabase) {
+      this.readall();    
+  }
+
+  readall(){
+    this.grilla=this.afDB.list('persona').valueChanges();
+  }
+  createpersona(){
+    this.afDB.database.ref('persona/'+this.json["codigo"]).set(this.json);
+    this.readall();
+    this.json={};
+    this.showform=false;
+  }
   addpersona(){
     this.showform=true;
     this.crearsh=true;
     this.editarsh=false;
   }
-  createpersona(){
-    this.grilla.push(this.json);   
-    this.json={}; 
-  }
+  
   cancel(){
-    this.json={};
-    this.showform=false;
+       this.json={};
+       this.showform=false;
+  
   }
   eliminar(x){
-    this.grilla.splice(this.grilla.indexOf(x),1);
+    
+    this.afDB.database.ref('persona/'+x["codigo"]).remove();
+    this.readall();
   }
   read(x){
+    console.log(x);
     this.json=x;
+    this.showform=true;
     this.crearsh=false;
     this.editarsh=true;
   }
   update(){
-    this.json={};    
+    this.afDB.database.ref('persona/'+this.json["codigo"]).set(this.json);
+    this.readall();
+    this.json={};
+    this.showform=false;
   }
+  // update(){
+  //   this.json={};    
+  // }
+  // createpersona(){
+  //   this.grilla.push(this.json);   
+  //   this.json={}; 
+  // }
+  // cancel(){
+  //   this.json={};
+  //   this.showform=false;
+  // }
+  // eliminar(x){
+  //   this.grilla.splice(this.grilla.indexOf(x),1);
+  // }
+
+  
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCKLkpcokvxfTie9lf45HSyeFaZMNGpHY0",
+  authDomain: "angular4-46d25.firebaseapp.com",
+  databaseURL: "https://angular4-46d25.firebaseio.com",
+  storageBucket: "angular4-46d25.appspot.com",
+  messagingSenderId: "829309376647"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
